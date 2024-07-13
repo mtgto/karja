2f5dd1947fa0835d7ecf9a64de9cab4db63b3f6a
mtgto <hogerappa@gmail.com>
2024-07-13 10:09:29 +0900

    Use SingleHostReverseProxy

--- cmd/karja/main.go
+++ cmd/karja/main.go
@@ -5,6 +5,9 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
 )
 
 //go:embed index.html
@@ -12,21 +15,31 @@ var html string
 
 type ReverseProxyService struct {
 	// TODO: Store information about connected other containers
+	proxy *httputil.ReverseProxy
 }
 
 func main() {
 	mux := http.NewServeMux()
-	service := &ReverseProxyService{}
+	otherContainerUrl, err := url.Parse("http://localhost:3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	proxy := httputil.NewSingleHostReverseProxy(otherContainerUrl)
+	service := &ReverseProxyService{proxy}
 	mux.Handle("/", service)
 	log.Fatal(http.ListenAndServe(":9000", mux))
 }
 
 func (s *ReverseProxyService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	html, err := template.New("index").Parse(html)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := html.Execute(w, nil); err != nil {
-		log.Fatal(err)
+	if strings.HasPrefix(r.Host, "test.") {
+		s.proxy.ServeHTTP(w, r)
+	} else {
+		html, err := template.New("index").Parse(html)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := html.Execute(w, nil); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
